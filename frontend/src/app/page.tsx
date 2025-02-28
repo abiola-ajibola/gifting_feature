f03da77c0208bfa5f29c49fb60bd8965ef20b104
Abiola <ajibolaabiolaahmed@gmail.com>
2025-02-28 19:33:00 +0100

    Disable static rendering of the home page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,10 @@
 import { ICardProps, PropertyCard } from "@/components/card";
 import { properties } from "@/services/properties";
+import { connection } from "next/server";
 
 export default async function Home() {
+  await connection();
   const propertiesData = await properties.getMany();
-  console.log({ propertiesData });
 
   return (
     <main className="flex flex-col gap-4 items-center min-h-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
